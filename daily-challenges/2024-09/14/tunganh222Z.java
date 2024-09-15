48d24f093a522a1a8894b765ddedac88943ca5f5
tunganh222Z <tunganh97tuaf@gmail.com>
2024-09-15 16:26:51 +0700

    tunganh222z_daily_challegens_1509

--- daily-challenges/2024-09/14/tunganh222Z.java
+++ daily-challenges/2024-09/14/tunganh222Z.java
@@ -12,68 +12,78 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
-public class tunganh222Z_1409_registerPage {
+public class tunganh222Z {
     WebDriver driver;
     WebDriverWait explicitWait;
-
+    // userName and email
     String userName = "TungAnh";
     String email = getEmailRand();
+
     @BeforeClass
-    public void beforeClass(){
+    public void beforeClass() {
         driver = new FirefoxDriver();
-        explicitWait = new WebDriverWait(driver,Duration.ofSeconds(15));
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(15));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
     }
 
-
     @Test
-    public void TC_01_registerPage(){
+    public void TC_01_registerPage() {
         driver.get("https://material.playwrightvn.com/");
         driver.findElement(By.xpath("//a[contains(text(),'Register Page')]")).click();
-
+        // locator
         By userName = By.xpath("//input[@id='username']");
         By email = By.xpath("//input[@id='email']");
+        //input information
         explicitWait.until(ExpectedConditions.visibilityOfElementLocated(userName));
         driver.findElement(userName).sendKeys(this.userName);
         driver.findElement(email).sendKeys(this.email);
-
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-
+        // Wait informationTale visible
         By informationTable = By.xpath("//tbody");
         explicitWait.until(ExpectedConditions.visibilityOfElementLocated(informationTable));
-
         List<WebElement> infor = driver.findElements(By.xpath("//tbody/tr/td"));
-        boolean checkUsername = false;
-        boolean checkEmail = false;
-
-        for (int i = 0 ; i < infor.size() ; i++){
-            String assertText = infor.get(i).getText();
-            if (assertText.contains(this.userName)){
-                checkUsername = true;
-            }
-            if (assertText.contains(this.email)){
-                checkEmail = true;
-            }
-        }
-
-        Assert.assertTrue(checkUsername);
-        Assert.assertTrue(checkEmail);
-
+        // Assert userName & email
+        Assert.assertEquals(infor.get(1).getText(), this.userName);
+        Assert.assertEquals(infor.get(2).getText(), this.email);
     }
 
+    @Test
+    public void TC_02_BMI() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vui lòng nhập số chiều cao m của bạn");
+        double height = sc.nextDouble();
+        System.out.println("Vui lòng nhập số cân nặng kg của bạn");
+        double weight = sc.nextDouble();
+        System.out.println("Phân loại :" + caculateBMI(valueBMI(weight, height)));
+        System.out.println("Kết quả BMI: " + valueBMI(weight, height));
+    }
 
+    public double valueBMI(double inputWeight, double inputHeight) {
+        return inputWeight / (inputHeight * inputHeight);
+    }
 
-    public String getEmailRand (){
+    public String caculateBMI(double BMI) {
+        if (BMI < 18.5 && BMI > 0) {
+            return "Gầy";
+        } else if (BMI >= 18.5 && BMI < 24.9) {
+            return "Bình thường";
+        } else if (BMI >= 25 && BMI < 29.9) {
+            return "Thừa cân";
+        } else {
+            return "Béo phì";
+        }
+    }
+
+    public String getEmailRand() {
         Random emailRand = new Random();
-
-        return "tunganh" + emailRand.nextInt(0,99999) + "@gmail.com";
-
+        return "tunganh" + emailRand.nextInt(0, 99999) + "@gmail.com";
     }
 
     @AfterClass
-    public void afterClass(){
+    public void afterClass() {
         driver.quit();
     }
 }
