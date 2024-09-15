f7bc81aa3a6177ce629c191af5e6577f68515193
Full name <dntthuan@gmail.com>
2024-09-16 01:59:12 +0700

    format code

--- daily-challenges/2024-09/16/dntthuan064.spec.ts
+++ daily-challenges/2024-09/16/dntthuan064.spec.ts
@@ -6,7 +6,7 @@ import { test, expect } from "@playwright/test";
  * Sau khi đảo ngược chuỗi, in ra kết quả.
  */
 async function reverseString(str: string): Promise<string> {
-    return [...str].reverse().join('')
+  return [...str].reverse().join('')
 }
 
 // Usage:
@@ -20,26 +20,26 @@ reverseString("thgirwyalphcavcov").then(result => console.log(result))
  * Kiểm tra kết quả đúng như thông tin đã điền.
  */
 test('thuna - 2024-09 day 16', async ({ page }) => {
-    await page.goto("https://material.playwrightvn.com/")
-    await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click()
+  await page.goto("https://material.playwrightvn.com/")
+  await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click()
 
-    const userName = "thuna"
-    const email = "thuna064@gmail.com"
-    const dateOfBirth = '1999-06-04'
+  const userName = "thuna"
+  const email = "thuna064@gmail.com"
+  const dateOfBirth = '1999-06-04'
 
-    await page.locator('#username').fill(userName)
-    await page.locator('#email').fill(email)
-    await page.locator('#male').click()
-    await page.locator('#reading').click()
-    await page.locator('#interests').selectOption('art')
-    await page.locator('#country').selectOption('uk')
-    await page.locator('#dob').fill(dateOfBirth)
-    await page.getByRole("button", { name: "Register" }).click()
+  await page.locator('#username').fill(userName)
+  await page.locator('#email').fill(email)
+  await page.locator('#male').click()
+  await page.locator('#reading').click()
+  await page.locator('#interests').selectOption('art')
+  await page.locator('#country').selectOption('uk')
+  await page.locator('#dob').fill(dateOfBirth)
+  await page.getByRole("button", { name: "Register" }).click()
 
-    await expect(page.locator('td:nth-child(2)')).toHaveText(userName)
-    await expect(page.locator('td:nth-child(3)')).toHaveText(email)
-    await expect(page.locator('td:nth-child(4)')).toContainText('Gender: male')
-    await expect(page.locator('td:nth-child(4)')).toContainText('Hobbies: reading')
-    await expect(page.locator('td:nth-child(4)')).toContainText('Country: uk')
-    await expect(page.locator('td:nth-child(4)')).toContainText(`Date of Birth: ${dateOfBirth}`)
+  await expect(page.locator('td:nth-child(2)')).toHaveText(userName)
+  await expect(page.locator('td:nth-child(3)')).toHaveText(email)
+  await expect(page.locator('td:nth-child(4)')).toContainText('Gender: male')
+  await expect(page.locator('td:nth-child(4)')).toContainText('Hobbies: reading')
+  await expect(page.locator('td:nth-child(4)')).toContainText('Country: uk')
+  await expect(page.locator('td:nth-child(4)')).toContainText(`Date of Birth: ${dateOfBirth}`)
 })
