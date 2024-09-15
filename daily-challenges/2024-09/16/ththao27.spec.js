8d89e64b97a00e5cc5a76d7db276346431edfe54
ththao27 <102850170+ththao27@users.noreply.github.com>
2024-09-15 22:54:35 +0700

    Delete daily-challenges/2024-09/16/ththao27.spec.js
    Remove file 16-09

--- daily-challenges/2024-09/16/ththao27.spec.js
+++
@@ -1,7 +0,0 @@
-function reverseString(str) {
-    return str.split('').reverse().join('');
-}
-
-const inputString = "ThaoThanh";
-const reversedString = reverseString(inputString);
-console.log("Chuỗi đảo ngược:", reversedString);
