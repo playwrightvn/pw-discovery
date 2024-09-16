18d09f8bb3b20ec6f7f0df9f6adffc44a9561182
ThaoTran <tranthanhthao2712@gmail.com>
2024-09-16 22:50:32 +0700

    add solution for exercise 09-16

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,8 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless: false,  
+    viewport: { width: 1280, height: 720 }, 
     trace: 'on',
   },
 
