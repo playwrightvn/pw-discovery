83ad5e148864e9102dfbc3c879267d6d5517f9c3
Phong Do <dominhphong306@gmail.com>
2024-09-20 23:10:53 +0700

    feat: add solution day 17 (#27)

--- daily-challenges/2024-09/17/playwrightvietnam.spec copy.ts
+++ daily-challenges/2024-09/17/playwrightvietnam.spec copy.ts
@@ -13,11 +13,11 @@ function isPrime(num: number): boolean {
   if (num < 2) {
     return false;
   }
-
+  
   if (num == 2) {
     return true;
   }
-
+  
   if (num % 2 == 0) {
     return false;
   }
@@ -62,7 +62,7 @@ test('2024-09 day 17', async ({ page }) => {
   await test.step('Thêm vào giỏ hàng 2 sản phẩm 1.', async () => {
     await page.locator(`[data-product-id="${products[0].id}"]`).click({ clickCount: products[0].quantity });
   });
-
+  
   await test.step('Thêm vào giỏ hàng 2 sản phẩm 2.', async () => {
     await page.locator(`[data-product-id="${products[1].id}"]`).click({ clickCount: products[1].quantity });
   });
@@ -71,40 +71,22 @@ test('2024-09 day 17', async ({ page }) => {
     await page.locator(`[data-product-id="${products[2].id}"]`).click({ clickCount: products[2].quantity });
   });
 
-  await test.step('Kiểm số lượng sản phẩm đúng.', async () => {
-    const totalQty = products[0].quantity + products[1].quantity + products[2].quantity;
+  const cartItems = page.locator('#cart-items');
 
-    // Kiểm tra có số lượng sản phẩm trong giỏ hàng
-    const rowLocator = page.locator("//tbody//tr");
-    await expect(rowLocator).toHaveCount(products.length);
+  let totalPrice = 0;
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    const row = i + 1;
+    const productTotalPrice = product.quantity * product.price;
+    totalPrice += productTotalPrice;
 
-    // Kiểm tra tổng số lượng sản phẩm
-    let cartTotalQty = 0;
-    const allRows = await rowLocator.all();
-    for (const row of allRows) {
-      const rawQty = await row.locator("//td").nth(2).textContent();
-      cartTotalQty += parseInt(rawQty || '');
-    }
+    await expect(cartItems.locator(`tr:nth-child(${row}) td:nth-child(1)`)).toHaveText(product.name);
+    await expect(cartItems.locator(`tr:nth-child(${row}) td:nth-child(2)`)).toHaveText(`$${product.price.toFixed(2)}`);
+    await expect(cartItems.locator(`tr:nth-child(${row}) td:nth-child(3)`)).toHaveText(product.quantity.toString());
+    await expect(cartItems.locator(`tr:nth-child(${row}) td:nth-child(4)`)).toHaveText(`$${productTotalPrice.toFixed(2)}`);
+  }
 
-    expect(cartTotalQty).toEqual(totalQty)
-  });
-
-  await test.step('(Nâng cao) Kiểm tra tổng tiền sản phẩm đúng (tổng tiền = tổng (số lượng * đơn giá)).', async () => {
-    let totalMoney = 0;
-    products.forEach(item => totalMoney += item.price * item.quantity);
-    const rowLocators = await page.locator("//tbody//tr").all();
-
-    let cartTotal = 0;
-    for (const row of rowLocators) {
-      const cells = await row.locator("//td").all();
-      const rawPrice = await cells[1].textContent() || '';
-      const rawQty = await cells[2].textContent() || '';
-
-      const itemPrice = parseInt(rawPrice.replace('$', ''));
-      const itemQty = parseInt(rawQty);
-      cartTotal += itemPrice * itemQty;
-    }
-
-    expect(cartTotal).toEqual(totalMoney)
-  });
+  // Assert total price in cart
+  expect(totalPrice).toBe(150);
+  await expect(page.locator('.total-price')).toHaveText(`$${totalPrice.toFixed(2)}`);
 });
