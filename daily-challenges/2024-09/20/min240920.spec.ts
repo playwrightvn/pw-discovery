c5765c1567e2e168a0f2a8a58a1ce5ef7ab08750
phuongnt7 <phuongnt7@smartosc.com>
2024-09-24 22:26:33 +0700

    fix min240920

--- daily-challenges/2024-09/20/min240920.spec.ts
+++ daily-challenges/2024-09/20/min240920.spec.ts
@@ -47,7 +47,8 @@ test('min240920', async ({ page }) => {
     await page.goto('https://material.playwrightvn.com/')
     await page.locator(`//a[normalize-space(text())='Bài học 5: Xử lý mouse event']`).click()
     
-    await page.click(`//div[@id='clickArea']`)
+    const clickArea = page.click(`//div[@id='clickArea']`);
+    await clickArea.click()
     await expect(page.locator(`//p[@id='clickCount']`)).toHaveText('Số lần nhấn: 1')
     await expect(page.locator(`//p[@id='clickType']`)).toHaveText('Loại nhấn: Đơn')
     await expect(page.locator(`//p[@id='modifierKeys']`)).toHaveText('Phím kèm theo: Không có')
