0a8bff1851a8f41fbbd3772b47b6306ee828d558
khanh5923 <tranquangkhanh050923@gmail.com>
2024-09-28 02:13:43 +0700

    feature: Update my solution for daily 18

--- daily-challenges/2024-09/18/khanh5923_18.spec.js
+++ daily-challenges/2024-09/18/khanh5923_18.spec.js
@@ -1,34 +1,34 @@
+// javascript
 function calculateAge(yearBirth){
     const yearCurrent = new Date().getFullYear();
     return yearBirth > yearCurrent ? console.log(`Nam sinh khong hop le`) : console.log(`Tuoi cua ban la: ${yearCurrent-yearBirth}`)
 }
 calculateAge(2003)
 
-// ---------------------------------------------------------
-
+// playwright
 import {test, expect} from '@playwright/test'
+
 test('bai4_18', async ({ page }) => {
-    await page.goto(`https://material.playwrightvn.com/`)
-    await page.click(`//a[contains(text(),'Bài học 3: Todo page')]`)
     const testData = {
         text: "Xin chào, đây là bài thực hành ngày 18 tháng 9",
         update: "Xin chào, đây là bài thực hành ngày 18 tháng 9 - phiên bản đã chỉnh sửa"
     }
+    await page.goto(`https://material.playwrightvn.com/`)
+    await page.click(`//a[contains(text(),'Bài học 3: Todo page')]`)
     await page.locator(`//input[@id='new-task']`).fill(testData.text)
     await page.locator(`//button[@id='add-task']`).click()
-    const onlyAdd = page.locator(`//ul[@id="task-list"]//li`)
-    // Verify chỉ có 1 Todo duy nhất được add vào.
-    expect(await onlyAdd.count()).toBe(1)
-    const text = onlyAdd.locator(`//span`)
-    // Verify noi dung.
-    await expect(text).toHaveText(testData.text)
+    const addTaskList = page.locator(`//ul[@id="task-list"]//li`)
+    expect(await addTaskList.count()).toBe(1) // Verify chỉ có 1 Todo duy nhất được add vào.
+    const text = addTaskList.locator(`//span`)
+    await expect(text).toHaveText(testData.text) // Verify noi dung.
+
     // Lắng nghe sự kiện dialog và nhập giá trị vào prompt
     page.on('dialog', async dialog => {
         await dialog.accept(testData.update); // Cung cấp giá trị mới cho prompt
     });
     await page.click(`//ul[@id="task-list"]//button[normalize-space()='Edit']`)
-    // Verify noi dung vua cap nhap.
-    await expect(text).toHaveText(testData.update)
+    await expect(text).toHaveText(testData.update) // Verify noi dung vua cap nhap.
+
     // Lắng nghe sự kiện dialog và xác định confirm accept() or dismiss()
     await page.locator(`//button[contains(@id,'-delete')]`).click()
     page.on('dialog', async dialog => {
@@ -36,6 +36,5 @@ test('bai4_18', async ({ page }) => {
             await dialog.accept();
         }
     });
-    // Verify da xoa
-    await expect(page.locator(`//ul[@id="task-list"]`)).toBeEmpty()
+    await expect(page.locator(`//ul[@id="task-list"]`)).toBeEmpty() // Verify da xoa
 });
