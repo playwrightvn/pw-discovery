6354b1df34703055693512ebe435e92cc3790061
thaoha93 <44538603+ThaoHaTB@users.noreply.github.com>
2024-09-28 12:38:34 +0700

    update format code

--- daily-challenges/2024-09/19/thaoha93.spec.ts
+++ daily-challenges/2024-09/19/thaoha93.spec.ts
@@ -5,7 +5,6 @@ const addProduct = (name: string, price: number): void => {
     productList[name] = price;
     console.log(`Thêm sản phẩm ${name}, giá ${price}.`);
 };
-
 const removeProduct = (name: string): void => {
     if (productList[name]) {
         delete productList[name];
@@ -14,7 +13,6 @@ const removeProduct = (name: string): void => {
         console.log(`${name} không tồn tại trong danh sách.`);
     }
 };
-
 const calculateTotal = (): number => {
     let total = 0;
     for (let price of Object.values(productList)) {
@@ -22,30 +20,25 @@ const calculateTotal = (): number => {
     }
     return total;
 };
-
 addProduct("Táo", 30);
 addProduct("Chuối", 15);
 addProduct("Cam", 20);
 addProduct("Ổi", 33);
 addProduct("Nhãn", 50);
 console.log("Tổng giá trị sản phẩm:", calculateTotal());
-
 removeProduct("Ổi");
 console.log("Tổng giá trị sau khi xoá:", calculateTotal());
 
-//Playwright
+// Playwright
 import {expect, test} from '@playwright/test';
-
 test('Day 19', async({page})=>{
     await page.goto('https://material.playwrightvn.com/');
     await page.locator('[href|="05"]').click();
-    let items = await page.locator('.puzzle-container').locator('div').count();
-    
+    let items = await page.locator('.puzzle-container').locator('div').count();   
     page.on('dialog', async dialog => {
         expect(dialog.message()).toBe("Congratulations! You completed the puzzle.");
         await dialog.dismiss(); 
-      });
-    
+      }); 
     for (let i=1;i<=items;i++) {
         const source = page.locator('.puzzle-container').locator(`#piece-${i}`);
         const target = page.locator('.dropzones-container').locator('.dropzone').nth(i-1) ;
