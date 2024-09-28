2f12a8ec605652921307279bbc0dff6aacb01782
thaoha93 <44538603+ThaoHaTB@users.noreply.github.com>
2024-09-28 12:56:08 +0700

    Day 19 (#61)

--- daily-challenges/2024-09/14/thao.ha93.spec.ts
+++ daily-challenges/2024-09/14/thao.ha93.spec.ts
@@ -22,28 +22,23 @@ function calculateBMI(weight: number, height: number) {
 let weight = 53, height = 1.5;
 calculateBMI(weight, height);
 
-//Playwright
+// Playwright
 import { expect, test } from '@playwright/test';
 test('Day 14', async ({ page }) => {
     await page.goto('https://material.playwrightvn.com/');
 
     await page.locator('[href|="01"]').click();
-
-    let username= 'ThaoHa';
+    let username = 'ThaoHa';
     const textName = await page.locator('#username');
     await textName.clear();
     await textName.fill(username);
-
-    let email='lily@gmail.com'
+    let email = 'lily@gmail.com'
     const txtemail = await page.locator('#email');
     await txtemail.clear();
     await txtemail.fill(email);
 
-    await page.locator('[type="submit"]').click();
-
-    await expect( page.locator('//*[@id="userTable"]//tbody/tr[1]/td[2]')).toHaveText(username);
-    await expect( page.locator('//*[@id="userTable"]//tbody/tr[1]/td[3]')).toHaveText(email);
-    
+    await expect(page.locator('//*[@id="userTable"]//tbody/tr[1]/td[2]')).toHaveText(username);
+    await expect(page.locator('//*[@id="userTable"]//tbody/tr[1]/td[3]')).toHaveText(email);
     // DEBUG PURPOSE ONLY
     await page.waitForTimeout(3 * 1000);
 })
