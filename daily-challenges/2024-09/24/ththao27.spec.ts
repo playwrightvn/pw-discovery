a39a0e8a5880a4bb68f72e9ef40d69f27f88fdde
ThaoTran <tranthanhthao2712@gmail.com>
2024-09-30 22:08:30 +0700

    update remove space

--- daily-challenges/2024-09/24/ththao27.spec.ts
+++ daily-challenges/2024-09/24/ththao27.spec.ts
@@ -7,7 +7,6 @@ function getCurrentDate() {
   const day = String(today.getDate()).padStart(2, '0');
   const month = String(today.getMonth() + 1).padStart(2, '0');
   const year = today.getFullYear();
-
   return `${day}/${month}/${year}`;
   }
   
