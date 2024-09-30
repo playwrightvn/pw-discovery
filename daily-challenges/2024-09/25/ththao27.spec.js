58b3391f1156c5bfc8db3dc228f4430dd82fc489
ThaoTran <tranthanhthao2712@gmail.com>
2024-09-30 21:37:15 +0700

    feat: update exercises on 25-09 25-09

--- daily-challenges/2024-09/25/ththao27.spec.js
+++ daily-challenges/2024-09/25/ththao27.spec.js
@@ -7,41 +7,24 @@ function trimSpaces(str) {
 }
 
 // output 1
-
 const str1= "   Xin   chào   mọi người   ";
 console.log(trimSpaces(str1));
 
 // output 2
-
 const str2= "   JavaScript    is    fun   ";
 console.log(trimSpaces(str2));
 
 // output 3
-
 const str3= "   Lập  trình   JavaScript   ";
 console.log(trimSpaces(str3));
 
 // Playwright
 
-test('ththao27 - 2024-09 day 25: Emulate permissions for location, camera, and microphone', async ({ }) => {
-  const browser = await chromium.launch({headless: false});
-  const context = await browser.newContext({
-    geolocation: {latitude: 21.0285, longitude: 105.8542},
-    permissions: ['geolocation']
+test.beforeEach(async ({ context }) => {
+  await context.grantPermissions(['geolocation', 'camera', 'microphone'], {
+      origin: 'https://material.playwrightvn.com'
   });
-
-  await context.grantPermissions(['geolocation', 'camera', 'microphone']);
-  const page = await context.newPage();
-  await page.goto('https://material.playwrightvn.com/017-detect-user-agent.html');
-  await page.waitForTimeout(3000);
-
-  const locationPermission = page.locator('//*[@id="locationPermission"]');
-  const cameraPermission = page.locator('//*[@id="cameraPermission"]');
-  const microphonePermission = page.locator('//*[@id="microphonePermission"]');
-
-  await expect(locationPermission).toHaveText('Đã cấp quyền');
-  await expect(cameraPermission).toHaveText('Đã cấp quyền');
-  await expect(microphonePermission).toHaveText('Đã cấp quyền');
-
-  await browser.close();
-});
+})
+test('ththao27: 2024-09-25', async ({ page }) => {
+  await page.goto("https://material.playwrightvn.com/017-detect-user-agent.html");
+})
