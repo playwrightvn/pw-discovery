d65d76e2a1ae3cf381bdde2fa46962cb7e2cb7d0
ThaoTran <tranthanhthao2712@gmail.com>
2024-10-12 15:25:51 +0700

    fix comments

--- daily-challenges/2024-10/08/ththao27.js
+++ daily-challenges/2024-10/08/ththao27.js
@@ -1,22 +1,25 @@
-function tinhNgayDenTet() {
-    const tet2025 = new Date('2025-01-29T00:00:00');
+function tinhNgayDenTet(tetDate = new Date('2025-01-29T00:00:00')) {
 
     const interval = setInterval(() => {
         const now = new Date();
-        const timeDifference = tet2025 - now;
+        const numberOfDay = tetDate - now;
 
-        if (timeDifference <= 0) {
+        if (numberOfDay <= 0) {
             clearInterval(interval);
             console.log("Happy new year!");
             return;
         }
+        const one_second = 1000;
+        const one_minute = 60 * one_second;
+        const one_hour = 60 * one_minute;
+        const one_day = 24 * one_hour;
 
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const days = Math.floor(numberOfDay / (one_day));
+        const hours = Math.floor((numberOfDay % (one_day)) / (one_hour));
+        const minutes = Math.floor((numberOfDay % (one_hour)) / (one_minute));
+        const seconds = Math.floor((numberOfDay % (one_minute)) / one_second);
 
-        console.log(`Chỉ còn ${days} ngày, ${hours} giờ, ${minutes} phút, ${seconds} giây nữa là đến Tết Nguyên Đán 2025`);
+        console.log(`Countdown to Tet 2025: ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
     }, 1000);
 }
 
