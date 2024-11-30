eb38e5cd58cf909d16f86027821dcb24d50ad545
ThaoTran <tranthanhthao2712@gmail.com>
2024-11-30 16:44:30 +0700

    add func decryptionCaesar

--- daily-challenges/2024-11/07/ththao27.spec.ts
+++ daily-challenges/2024-11/07/ththao27.spec.ts
@@ -1,14 +1,13 @@
-import { test } from '@playwright/test'
 function caesarCipher (massages: string[], shift: number): string[] {
     shift = shift % 26;
     const result: string[] = [];
     for (const item of massages) {
-        result.push(Encryption(item, shift));
+        result.push(encryptionCaesar(item, shift));
     }
     return result;
 }
 
-function Encryption (massage: string, shift: number): string {
+function encryptionCaesar (massage: string, shift: number): string {
     let result = '';
     for (let char of massage) {
         if (char >= 'A' && char <= 'Z') {
@@ -25,7 +24,47 @@ function Encryption (massage: string, shift: number): string {
     }
     return result;
 }
-console.log(caesarCipher(["Happy coding!"], 3));
-console.log(caesarCipher(["HELLO"], -1));
-console.log(caesarCipher(["test"], 27));
-console.log(caesarCipher(["Hello, World 123!"], 1));
+
+//1. **Thêm chức năng giải mã:**
+
+function caesarDecipher (massages: string[], shift: number): string[] {
+    shift = shift % 26;
+    const result: string[] = [];
+    for (const item of massages) {
+        result.push(decryptionCaesar(item, shift));
+    }
+    return result;
+}
+
+function decryptionCaesar (massage: string, shift: number): string {
+    let result = '';
+    for (let char of massage) {
+        if (char >= 'A' && char <= 'Z') {
+            let newChar = String.fromCharCode((char.charCodeAt(0) - 65 - shift) % 26 + 65);
+            result += newChar;
+        }
+        else if (char >= 'a' && char <= 'z') {
+            let newChar = String.fromCharCode((char.charCodeAt(0) - 97 - shift) % 26 + 97);
+            result += newChar;
+        }
+        else {
+            result += char;
+          }
+    }
+    return result;
+}
+
+// //2. **Thêm tính năng tự động đoán shift:**
+// function breakCaesar(encodedMessage: string): number {
+//     let shift: number = 0;
+//     for (let char of encodedMessage) {
+//         if (char >= 'A' && char <= 'Z') {
+
+//         }
+//     }
+// }
+
+
+console.log(caesarDecipher(["Khoor Zruog!", "FDHVDU"], 3));
+console.log(caesarDecipher(["GDKKN"], -1));
+console.log(caesarDecipher(["uftu"], 27));
