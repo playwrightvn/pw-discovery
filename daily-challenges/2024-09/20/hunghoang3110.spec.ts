4309147c848414f90c2804f41cd1011c3e3205a0
hunghoang3110 <hunghoang3110@gmail.com>
2024-12-10 22:31:36 +0700

    feat: add solution for day 20

---
+++ daily-challenges/2024-09/20/hunghoang3110.spec.ts
@@ -0,0 +1,46 @@
+// Javascript
+function findLargestNumber(array) {
+
+    if (!Array.isArray(array) || array.length === 0) {
+        throw new Error('Input must be a non-empty array');
+    }
+    if (!array.every(num => typeof num === 'number')) {
+        throw new Error('All elements must be numbers');
+    }
+    const maxNumber = Math.max(...array);
+    return `Maximum number is : ${maxNumber}`;
+
+}
+
+// Playwright
+import { test, expect } from '@playwright/test'
+
+test('mouse event', async ({ page }) => {
+
+    // Navigate to Mouse event page
+    await page.goto('https://material.playwrightvn.com/');
+    await expect(page).toHaveURL(/.*playwrightvn.com/);
+    await page.getByRole('link', { name: /mouse event/ }).click();
+    await expect(page).toHaveURL(/.*mouse/);
+
+    const CLICK_TARGET = page.getByText('Nhấn hoặc nhấn đúp vào đây!');
+
+    //Click
+    await CLICK_TARGET.click();
+    await expect(page.getByText(/số lần nhấn: 1/i)).toBeVisible();
+    await expect(page.getByText(/loại nhấn: đơn/i)).toBeVisible();
+    await expect(page.getByText(/phím kèm theo: không có/i)).toBeVisible();
+
+    // Double click
+    await CLICK_TARGET.dblclick();
+    await expect(page.getByText(/số lần nhấn: 3/i)).toBeVisible();
+    await expect(page.getByText(/loại nhấn: đúp/i)).toBeVisible();
+    await expect(page.getByText(/phím kèm theo: không có/i)).toBeVisible();
+
+    // shift and click
+    await CLICK_TARGET.click({ modifiers: ['Shift'] });
+    await expect(page.getByText(/số lần nhấn: 4/i)).toBeVisible();
+    await expect(page.getByText(/loại nhấn: đơn/i)).toBeVisible();
+    await expect(page.getByText(/phím kèm theo: Shift/i)).toBeVisible();
+
+})
