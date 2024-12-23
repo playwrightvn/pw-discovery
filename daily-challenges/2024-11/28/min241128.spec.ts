40ab28e7ca2edad2f0df8323bd48056b8aeafaee
phuongnt7 <phuongnt7@smartosc.com>
2024-12-23 19:51:09 +0700

    min241128

--- daily-challenges/2024-11/28/min241128.spec.ts
+++ daily-challenges/2024-11/28/min241128.spec.ts
@@ -9,26 +9,27 @@ Hãy viết test cho đề bài sau:
 - Chọn đúng pokemon theo yêu cầu. Khi tìm được Pokemon, hãy dừng lại.
 */
 
-import {test, expect} from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
-test('Find Pokemon', async ({ page }) => {
-    const regex = /Tìm Pokemon: (.+)/
-    await page.goto(`https://material.playwrightvn.com/`)
-    await page.locator(`//a[normalize-space(text())='Game 04: Túi mù pokemon']`).click()
-    await page.waitForLoadState()
-    let pokemon = await page.locator(`//div[@id="target-pokemon"]`).innerText()
-    const targetName = pokemon.slice(pokemon.indexOf(': ') + 2).trim()
-    const numberOfBags = await page.locator(`//div[@class="mystery-bags"]/div`).count()
-    for(let i = 1; i <= numberOfBags; i++){
-        await page.locator(`//div[@class="mystery-bags"]/div[${i}]`).click()
-        await page.waitForLoadState()
-        let bag = await page.locator(`//div[@class="bag opened"][${i}]//div[@class="pokemon-name"]`).innerText()
-        if(bag == targetName){
-            await expect(page.locator(`//div[@id='result']`)).toHaveText(`Chúc mừng! Bạn đã tìm thấy ${targetName}!`)
-            break
-        } 
-        if(bag != targetName){
-            await expect(page.locator(`//div[@id='result']`)).toHaveText(`Tiếp tục tìm kiếm! Đây là ${bag}`)
-        }
+test("Find Pokemon", async ({ page }) => {
+    const regex = /Tìm Pokemon: (.+)/;
+    await page.goto(`https://material.playwrightvn.com/`);
+    await page.locator(`//a[normalize-space(text())='Game 04: Túi mù pokemon']`).click();
+    await page.waitForLoadState();
+    let pokemon = await page.locator(`//div[@id="target-pokemon"]`).innerText();
+    const targetName = pokemon.slice(pokemon.indexOf(": ") + 2).trim();
+    const numberOfBags = await page.locator(`//div[@class="mystery-bags"]/div`).count();
+    for (let i = 1; i <= numberOfBags; i++) {
+        await page.locator(`//div[@class="mystery-bags"]/div[${i}]`).click();
+        await page.waitForLoadState();
+        let bag = await page.locator(`//div[@class="bag opened"][${i}]//div[@class="pokemon-name"]`).innerText();
+        if (bag == targetName) {
+            i = numberOfBags;
+        await expect(page.locator(`//div[@id='result']`)).toHaveText(`Chúc mừng! Bạn đã tìm thấy ${targetName}!`);
+        break;
     }
-})
+    if (bag != targetName) {
+      await expect(page.locator(`//div[@id='result']`)).toHaveText(`Tiếp tục tìm kiếm! Đây là ${bag}`);
+    }
+  }
+});
