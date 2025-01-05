e5df275abbea05cec74e86f07c10947c488ca851
hunghoang3110 <hunghoang3110@gmail.com>
2025-01-05 14:52:59 +0700

    Fixed comments from merge request

--- daily-challenges/2024-09/25/hunghoang3110.spec.ts
+++ daily-challenges/2024-09/25/hunghoang3110.spec.ts
@@ -10,28 +10,25 @@ function trimSpace(input) {
 }
 
 // Playwright
-import { test, expect } from '@playwright/test';
-
+import { test, expect, page } from '@playwright/test';
 test('should verify browser permissions are granted for geolocation, camera and microphone', async ({ context }) => {
-
-    // overwrite the location for this test
+    // Overwrite the location, camera and microphone permissions for this test
     await context.grantPermissions(['geolocation', 'camera', 'microphone']);
     const page = await context.newPage();
     await page.goto('https://material.playwrightvn.com/');
     await expect(page).toHaveTitle('Tài liệu học automation test - Playwright Việt Nam');
     await page.getByRole('link', { name: /Detect user agent/ }).click();
     await expect(page).toHaveTitle('Detect User Agent');
-
-    //Verify
-    async function verifyPermission(permissionName: string) {
-        await expect(page
-            .locator('tr')
-            .filter({ has: page.getByRole('cell', { name: permissionName }) })
-            .locator('td:nth-child(2)'))
-            .toHaveText('Đã cấp quyền');
-    }
-    await verifyPermission('Location');
-    await verifyPermission('Camera');
-    await verifyPermission('Microphone');
-
+    // Verify
+    await verifyPermission(page, 'Location');
+    await verifyPermission(page, 'Camera');
+    await verifyPermission(page, 'Microphone');
 });
+
+async function verifyPermission(page: page, permissionName: string) {
+    await expect(page
+        .locator('tr')
+        .filter({ has: page.getByRole('cell', { name: permissionName }) })
+        .locator('td:nth-child(2)'))
+        .toHaveText('Đã cấp quyền');
+}
