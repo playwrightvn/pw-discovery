cd0d2841c3e4fa86d41dbd2a1fc8c634d213f324
Phong Do Minh <do.minh.phong@outpost24.com>
2025-05-08 13:34:44 +0700

    feat: add example about table

---
+++ community-support/010-table-filter.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from '@playwright/test';
+
+test('example filter', async ({ page }) => {
+    await page.setContent(
+        `
+<table id="single_table" border="1" cellpadding="8" cellspacing="0">
+  <thead>
+    <tr>
+      <th>STT</th>
+      <th>Tên ca sĩ</th>
+      <th>Bài hát hay</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Sơn Tùng</td>
+      <td>Lạc Trôi</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Mỹ Tâm</td>
+      <td>Ước Gì</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Đen Vâu</td>
+      <td>Trốn Tìm</td>
+    </tr>
+  </tbody>
+</table>
+        `
+    );
+
+    const table = page.locator("#single_table");
+    const rows = table.getByRole("row");
+    const targetRow = rows.filter({ hasText: 'Sơn Tùng' });
+    await expect(targetRow).toContainText("Lạc Trôi");
+})
